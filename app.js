b4d219d7f1f199be01c393ff60630499ab958f9c
Stéphanie Rachel <stephrachel12@gmail.com>
2024-09-05 11:56:29 -0300

    update

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
 let section = document.getElementById("resultados-pesquisa");
-console.log(section);
+let botao = document.getElementById("botao")
 
-let resultados = "";
+botao.addEventListener("click", () => {
+    let resultados = "";
 
-for (let dado of mangas) {
+    for (let dado of mangas) {
     resultados += `
-    <div class="item-resultado">
-        <div>
-            <img src="${dado.capa}" alt="${dado.alt}">
-        </div>
+        <div class="item-resultado">
+            <div>
+                <img src="${dado.capa}" alt="${dado.alt}">
+            </div>
 
-        <div>
-            <h2>${dado.titulo}</h2>
+            <div>
+                <h2>${dado.titulo}</h2>
 
-            <p class="descricao-meta">${dado.descricao}</p>
-            <div class="infos">
-                <div>
-                    <p class="autor">Escrita por: ${dado.escritor}</p>
-                    <p class="autor">Desenhada por: ${dado.desenhista}</p>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <div class="infos">
+                    <div>
+                        <p class="autor">Escrita por: ${dado.escritor}</p>
+                        <p class="autor">Desenhada por: ${dado.desenhista}</p>
+                    </div>
+                    <p class="link"><a href="${dado.link}" target="_blank">Mais informações sobre a obra</a></p>
                 </div>
-                <p class="link"><a href="${dado.link}" target="_blank">Mais informações sobre a obra</a></p>
             </div>
         </div>
-    </div>
     `
 }
 
 section.innerHTML = resultados
+})
